1cf1331f8a7098f18e891a2c86472db993585bc7
YuSuhwa-ve <dhfhfkzjxms@sookmyung.ac.kr>
2023-05-06 14:28:11 +0900

    init : 4차 세미나 스프링 프로젝트 세팅

---
+++ FourthSeminar/src/main/java/sopt/org/FourthSeminar/FourthSeminarApplication.java
@@ -0,0 +1,13 @@
+package sopt.org.FourthSeminar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FourthSeminarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FourthSeminarApplication.class, args);
+	}
+
+}
