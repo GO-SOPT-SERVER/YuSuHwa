d7cf07fa2214ed620e2613d668f834f1c7ab0e67
YuSuhwa-ve <dhfhfkzjxms@sookmyung.ac.kr>
2023-06-21 05:28:07 +0900

    feat : 7차 세미나 기본과제 : 이미지 여러장 업로드

--- FourthSeminar/src/main/java/sopt/org/FourthSeminar/controller/BoardController.java
+++ FourthSeminar/src/main/java/sopt/org/FourthSeminar/controller/BoardController.java
@@ -7,12 +7,14 @@ import org.springframework.web.bind.annotation.*;
 import sopt.org.FourthSeminar.common.dto.ApiResponse;
 import sopt.org.FourthSeminar.config.jwt.JwtService;
 import sopt.org.FourthSeminar.config.resolver.UserId;
+import sopt.org.FourthSeminar.controller.dto.request.BoardImageListRequestDto;
 import sopt.org.FourthSeminar.controller.dto.request.BoardRequestDto;
 import sopt.org.FourthSeminar.exception.Success;
 import sopt.org.FourthSeminar.external.client.aws.S3Service;
 import sopt.org.FourthSeminar.service.BoardService;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -21,18 +23,27 @@ public class BoardController {
 
     private final S3Service s3Service;
     private final BoardService boardService;
-    private final JwtService jwtService;
+    //private final JwtService jwtService;
 
     @PostMapping(value = "/create", consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.CREATED)
     public ApiResponse create(
             //@RequestHeader("Authorization") String accessToken,
             @UserId Long userId,
-            @ModelAttribute @Valid final BoardRequestDto request) {
+            //@ModelAttribute @Valid final BoardRequestDto request //단일 이미지
+            @ModelAttribute @Valid final BoardImageListRequestDto request ) { //여러 이미지
 
         //boardService.create(Long.parseLong(jwtService.getJwtContents(accessToken)),request);
-        String boardThumbnailImageUrl = s3Service.uploadImage(request.getThumbnail(), "board");
-        boardService.create(userId, boardThumbnailImageUrl, request);
+        //String boardThumbnailImageUrl = s3Service.uploadImage(request.getThumbnail(), "board");
+        List<String> boardThumbnailImageUrlList = s3Service.uploadImages(request.getBoardImages(), "board");
+
+        //boardService.create(userId, boardThumbnailImageUrl, request);
+        boardService.create(userId, boardThumbnailImageUrlList, request);
+
         return ApiResponse.success(Success.CREATE_BOARD_SUCCESS);
     }
+
+
+
+
 }
--- FourthSeminar/src/main/java/sopt/org/FourthSeminar/domain/Board.java
+++ FourthSeminar/src/main/java/sopt/org/FourthSeminar/domain/Board.java
@@ -19,8 +19,8 @@ public class Board extends AuditingTimeEntity{
     @JoinColumn(name = "user_id", nullable = false, foreignKey = @ForeignKey(ConstraintMode.CONSTRAINT))
     private User user;
 
-    @Column
-    private String thumbnail;
+//    @Column
+//    private String thumbnail;
 
     @Column(nullable = false)
     private String title;
@@ -31,16 +31,15 @@ public class Board extends AuditingTimeEntity{
     @Column(nullable = false)
     private Boolean isPublic;
 
-    private Board(User user, String thumbnail,String title, String content, Boolean isPublic) {
+    private Board(User user,String title, String content, Boolean isPublic) {
         this.user = user;
-        this.thumbnail=thumbnail;
         this.title =title;
         this.content = content;
         this.isPublic = isPublic;
     }
 
-    public static Board newInstance(User user, String thumbnail,String title, String content, Boolean isPublic) {
-        return new Board(user, thumbnail,title, content, isPublic);
+    public static Board newInstance(User user,String title, String content, Boolean isPublic) {
+        return new Board(user, title, content, isPublic);
     }
 
 }
